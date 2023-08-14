8f1e9f4b9da1b1342eb7a26c5b555abf3243d051
Zury Martinez <zury.martinez.saldivia@gmail.com>
2023-08-14 13:36:41 -0400

    Creation of authentication components and pages.
    The header was created with its respective design and added login verification functionality.
    protectedRoute.js was created to keep the home route protected, preventing access from other sources.
    The login and register pages were created with their respective logic and design following the defined color palette.
    Authcontext.js was created to establish an authorization formula and verify the user's login status within the application, allowing access to the main page. This serves as an authentication method.
    Specific routes were imported for each page and component.

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h2>Welcome to the Home Page!</h2>
+    </div>
+  );
+}
+
+export default Home;
